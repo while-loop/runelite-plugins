a7a4534dcb2d327243594546c4a0e23a7bfb4fc5
Anthony Alves <anthony.alves@rho.ai>
2020-03-06 17:45:16 -0500

    init zulrah helper

--- src/main/java/com/zulrahhelper/ZulrahHelperConfig.java
+++ src/main/java/com/zulrahhelper/ZulrahHelperConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.zulrahhelper;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface ZulrahHelperConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
