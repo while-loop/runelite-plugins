3b2a2939f29bee006f5c49a926ff853fba4855ba
Anthony <cvballa3g0@gmail.com>
2020-05-27 10:08:27 -0400

    apply phase img rotation in processing method

--- src/main/java/com/zulrahhelper/ui/ZulrahHelperPhasePanel.java
+++ src/main/java/com/zulrahhelper/ui/ZulrahHelperPhasePanel.java
@@ -1,6 +1,7 @@
 package com.zulrahhelper.ui;
 
 import com.zulrahhelper.Phase;
+import com.zulrahhelper.ZulrahHelperConfig;
 import com.zulrahhelper.ZulrahHelperPlugin;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.util.ImageUtil;
@@ -24,11 +25,6 @@ public class ZulrahHelperPhasePanel extends JPanel implements MouseListener {
         this.phase = phase;
 
         BufferedImage img = processImg(phase.getImage(plugin.getConfig()), rows);
-        final double theta = plugin.getConfig().imageOrientation().getRotation();
-        if (theta != 0) {
-            img = ImageUtil.rotateImage(img, theta);
-        }
-
         phaseIcon = new ImageIcon(img);
         phaseIconHover = new ImageIcon(ImageUtil.luminanceScale(img, .75f));
 
@@ -56,6 +52,12 @@ public class ZulrahHelperPhasePanel extends JPanel implements MouseListener {
         if (phase.isCompleted()) {
             img = ImageUtil.luminanceScale(img, 0.20f);
         }
+
+        final double theta = plugin.getConfig().imageOrientation().getRotation();
+        if (theta != 0) {
+            img = ImageUtil.rotateImage(img, theta);
+        }
+
         return img;
     }
 
